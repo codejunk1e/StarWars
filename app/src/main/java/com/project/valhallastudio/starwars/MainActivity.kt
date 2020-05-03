ea05d26eaedfb18608c6f4cb2e7f5b531186ea11
Robin <emeka14real@gmail.com>
2020-05-03 23:49:20 +0100

    Initial Commit

---
+++ app/src/main/java/com/project/valhallastudio/starwars/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.project.valhallastudio.starwars
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/project/valhallastudio/starwars/SplashScreen.kt
@@ -0,0 +1,38 @@
+package com.project.valhallastudio.starwars
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setImmersiveMode()
+        scheduleSplashScreen()
+    }
+    private fun setImmersiveMode() {
+        window.decorView.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+
+    private fun scheduleSplashScreen() {
+        val splashScreenDuration = getSplashScreenDuration()
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }, splashScreenDuration)
+
+    }
+    private fun getSplashScreenDuration() = 2400L
+}
